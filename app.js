bfbb30f3f563dcb7ce2e8d5ad83dff8b3700953c
anubhav1011 <gupta.anu@husky.neu.edu>
2018-03-05 22:02:59 -0500

    Updating app.js

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 function greet(){
-
-
     console.log('Hi');
-
-
 }
 
 greet();
 
 
 function logGreeting(fn){
-
-
     fn();
 }
 
 
 logGreeting(greet);
+
+var greetMe = function(){
+    console.log('Hi Anubhav');
+}
+
+
+logGreeting(greetMe);
+
+
+logGreeting(function(){
+
+    console.log('Hi from');
+});
