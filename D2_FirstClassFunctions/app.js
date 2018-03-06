79baa92076c6568d8b86ba0ed1faff415ac21609
anubhav1011 <gupta.anu@husky.neu.edu>
2018-03-05 21:44:45 -0500

    Updating function expressions log statement

--- D2_FirstClassFunctions/app.js
+++ D2_FirstClassFunctions/app.js
@@ -1,18 +1,18 @@
 // function statement
 function greet() {
-	console.log('hi');
+    console.log('hi');
 }
 greet();
 
 // functions are first-class
 function logGreeting(fn) {
-	fn();
+    fn();
 }
 logGreeting(greet);
 
 // function expression
-var greetMe = function() {
-	console.log('Hi Tony!');
+var greetMe = function () {
+    console.log('Hi Anubhav!');
 }
 greetMe();
 
@@ -20,6 +20,6 @@ greetMe();
 logGreeting(greetMe);
 
 // use a function expression to create a function on the fly
-logGreeting(function() {
-	console.log('Hello Tony!');
+logGreeting(function () {
+    console.log('Hello Anubhav!');
 });
